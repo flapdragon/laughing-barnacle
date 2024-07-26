5076d4a5f7bd6840f9eac4f009caee41e97f0778
David Dean <vermicious.knid.d@gmail.com>
2024-07-25 21:53:59 -0700

    Adds tailwind login form.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './Home'
+import Login from './Login'
+
 import './App.css'
 
 function App() {
   return (
-    <div>
-      Cool App
+    <div className="bg-slate-800">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   )
 }
