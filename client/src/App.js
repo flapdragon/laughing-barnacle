ad907fe843630357e1b3000ab3010d45558f3716
David Dean <vermicious.knid.d@gmail.com>
2024-07-31 20:52:06 -0700

    Adds session storage to signup and login.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './Home'
 import Login from './Login'
+import Signup from './Signup'
 
 import './App.css'
 
 function App() {
   return (
-    <div className="bg-slate-800">
+    <div className="h-screen w-screen bg-slate-800">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
   )
